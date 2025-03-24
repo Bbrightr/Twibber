a31848bc2124247dc5ff93c16dbede8627702e1b
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2025-03-24 21:19:18 +0100

    replies updated

--- index.js
+++ index.js
@@ -64,6 +64,25 @@ function getFeedHtml(){
             retweetedIconClass = 'retweeted'
         }
 
+        let repliesHtml = ''
+
+        if (tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml += `
+                            <div class="tweet-reply">
+                                <div class="tweet-inner">
+                                        <img src="${reply.profilePic}" class="profile-pic">
+                                        <div>
+                                            <p class="handle">${reply.handle}</p>
+                                            <p class="tweet-text">${reply.tweetText}</p>
+                                        </div>
+                                </div>
+                            </div>
+                `
+            })
+            // console.log(tweet.uuid)
+        }
+
         feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -93,6 +112,9 @@ function getFeedHtml(){
             </div>   
         </div>            
     </div>
+    <div class="hidden" id="replies-tweet.uuid">
+        ${repliesHtml}
+    </div> 
 </div>
 `
    })
